f2e3eac70d43b0a654559ea08d52f1167655bbc3
Tharusha Nawod <tharushanawod777@gmail.com>
2026-06-01 15:33:02 +0530

    change added

--- routes/contact.js
+++ routes/contact.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET contact page. */
-router.get('/', (req, res) => {
-  res.render('contact', {
-    title: 'Contact Us',   
+router.get("/", (req, res) => {
+  res.render("contact", {
+    title: "Contact Us",
   });
 });
 
 module.exports = router;
+
+//updated
